965624b50c320a9e46b169834cd0ef869c9ee8cf
SinanKocakDev <sinankocakdev@gmail.com>
2024-09-11 23:10:04 +0300

    fetc post

--- app/(routes)/blog/[slug]/page.tsx
+++ app/(routes)/blog/[slug]/page.tsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React from 'react'
 
-interface BrowseDetailPageProps {
-  params: {
-    slug: string;
-  };
+interface BrowseDetailPageProps{
+    params: {
+        slug: string
+    }
 }
 
 interface Post {
-  id: string;
+  _id: string;
   title: string;
-  date: string;
-  content: string;
-  author: string;
+  body: string;
+  user: string;
+  createdAt: string;
 }
 
 const BrowseDetailPage = async ({ params }: BrowseDetailPageProps) => {
-  let data = await fetch(`https://api.vercel.app/blog/${params.slug}`);
-  let post: Post = await data.json();
-  
-  return (
-    <div>
-      <span key={post.id}>
+  try {
+    const data = await fetch(`https://nextblog-backend.onrender.com/api/posts/${params.slug}`);
+
+    if (!data.ok) {
+      throw new Error('Veri alınamadı');
+    }
+    
+    const response = await data.json();
+    
+    if (!response.posts) {
+      throw new Error('Post bulunamadı');
+    }
+
+    const post: Post = response.posts;
+
+    return (
+      <div>
+        <span>{post._id}</span>
         <h1>{post.title}</h1>
-        <p>{post.content}</p>
-        <span>{post.author}</span>
-        <span>{post.date}</span>
-      </span>
-    </div>
-  );
-};
+        <p>{post.body}</p>
+        <span>{post.user}</span>
+      </div>
+    );
+  } catch (error) {
+    console.error("Veri alma sırasında hata:", error); // Hatanın detayını konsola yazdır
+    return <div>Bir hata oluştu: {String(error)}</div>;
+  }
+}
 
 export default BrowseDetailPage;
--- app/(routes)/blog/page.tsx
+++ app/(routes)/blog/page.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
 interface Post {
-  id: string;
+  _id: string;
   title: string;
-  date: string;
 }
 
 const BlogPage = async () => {
-    let data = await fetch('https://api.vercel.app/blog')
-    let posts: Post[] = await data.json();
+    let data = await fetch('https://nextblog-backend.onrender.com/api/posts');
+    let response = await data.json();
+    let posts: Post[] = response.posts;
+    
     return (
-      <ul>
+      <ul className='flex flex-col justify-between items-center space-y-4'>
         {posts.map((post) => (
-          <li key={post.id}><Link href={`/blog/${post.id}`}>{post.title}</Link></li>
+          <li key={post._id}>
+            <Link href={`/blog/${post._id}`}>{post.title}</Link>
+          </li>
         ))}
       </ul>
-    )
+    );
 }
 
-export default BlogPage
+export default BlogPage;
